7fdc61af0b0d07fc79b3bf12ef2ba9e992d2f105
nafeeur10 <nafeeur.kuet.cse6@gmail.com>
2020-06-11 20:08:04 +0600

    Sign Up added

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,30 +4,51 @@
 
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Login Here:</h5>
+        <ul class="nav nav-pills" id="pills-tab" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Sign Up</a>
+            </li>
+          </ul>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        
-        <div class="d-flex flex-column text-center">
 
-          <form>
-            <div class="form-group">
-              <input type="email" class="form-control" id="email1" placeholder="Your email address...">
+          <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                <div class="d-flex flex-column text-center">
+                    <form>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="email1" placeholder="Your email address...">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="password1" placeholder="Your password...">
+                        </div>
+                        <button type="button" class="btn btn-info btn-block btn-round">Login</button>
+                    </form>
+                </div>
             </div>
-            <div class="form-group">
-              <input type="password" class="form-control" id="password1" placeholder="Your password...">
+            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                <div class="d-flex flex-column text-center">
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="name" placeholder="Your Name here...">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="email1" placeholder="Your email address...">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="password1" placeholder="Your password...">
+                        </div>
+                        <button type="button" class="btn btn-info btn-block btn-round">Register</button>
+                    </form>
+                </div>
             </div>
-            <button type="button" class="btn btn-info btn-block btn-round">Login</button>
-          </form>
-    
-        </div>
-
-        <div class="modal-footer d-flex justify-content-center">
-            <div class="signup-section">Not a member yet? <a href="#a" class="text-info"> Sign Up</a>.</div>
-        </div>
+          </div>
 
       </div>
     </div>
